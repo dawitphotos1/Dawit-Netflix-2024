8781d7d503c7b6c5c1dea4da3e812663d928d210
Dawit <dawitphotos1@gmail.com>
2024-09-13 21:53:59 -0400

    upadte vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/',
+  base: "/Netflix-Clone-Dawit-2024/",
   plugins: [react()],
-})
+});
